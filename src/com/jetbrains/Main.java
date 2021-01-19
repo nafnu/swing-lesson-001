92b2ca68d5077778e5af285fabd7586b8aff0fdd
Nathalie Flores <21250@student.dorset-college.ie>
2021-01-19 07:26:10 -0600

    second commit

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,8 +1,25 @@
+
 package com.jetbrains;
+
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                JFrame frame = new JFrame("Hello World");
+
+                frame.setSize(600,500);
+
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                frame.setVisible(true);
+            }
+        });
     }
 }
